ac09f828dd0dea4070c6adc8309442086759853a
Felix Roos <roos@entrecode.de>
2015-12-14 17:59:35 +0100

    now using webGL to render flakes!

--- angular-snow.js
+++ angular-snow.js
@@ -6,17 +6,29 @@ angular.module('angular-snow', []).directive('ngSnow', function() {
 
   var renderer, scene, camera, cameraRadius = 50.0, cameraTarget, cameraX = 0, cameraY = 0, cameraZ = cameraRadius, particleSystem, particleSystemHeight = 100.0, clock, controls, parameters, onParametersUpdate, texture;
 
-  function init(el, flake, flakeCount, mouseControls, scale) {
+  var windowWidth = window.innerWidth;
+  var windowHeight = window.innerHeight;
 
+  /*  function clearCanvas() {
+   if (renderer&&renderer.domElement) {
+   renderer.domElement.clearRect(0, 0, windowWidth, windowHeight);
+   }
+   }*/
+
+  function init(el, flake, flakeCount, mouseControls, gui, scale) {
+    if (renderer) {
+      renderer.clear();
+    }
     renderer = new THREE.WebGLRenderer({alpha: true});
-
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    //renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(windowWidth, windowHeight);
 
     scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
+    camera = new THREE.PerspectiveCamera(45, windowWidth / windowHeight, 1, 10000);
     cameraTarget = new THREE.Vector3(0, 0, 0);
 
     texture = THREE.ImageUtils.loadTexture(flake);
+    texture.flipY = false;
 
     var numParticles  = flakeCount, width = 100, height = particleSystemHeight, depth = 100, parameters = {
       color:   0xFFFFFF,
@@ -107,9 +119,11 @@ angular.module('angular-snow', []).directive('ngSnow', function() {
     renderer.domElement.style.position = 'absolute';
     renderer.domElement.style.top = '0';
     renderer.domElement.style.left = '0';
-    renderer.domElement.style.zIndex = '-1';
-    //el.append(renderer.domElement);
-    document.body.appendChild(renderer.domElement);
+    renderer.domElement.style.width = '100%';
+    renderer.domElement.style.height = '100%';
+    renderer.domElement.style.pointerEvents = 'none';
+    el.append(renderer.domElement);
+    //document.body.appendChild(renderer.domElement);
 
     onParametersUpdate = function(v) {
       systemMaterial.uniforms.color.value.set(parameters.color);
@@ -122,34 +136,36 @@ angular.module('angular-snow', []).directive('ngSnow', function() {
       systemMaterial.uniforms.speedH.value = parameters.speedH;
       systemMaterial.uniforms.speedV.value = parameters.speedV;
     };
-    controls = new dat.GUI();
-    controls.close();
+    if (gui) {
+      controls = new dat.GUI();
+      controls.close();
 
-    controls.addColor(parameters, 'color').onChange(onParametersUpdate);
-    controls.add(parameters, 'height', 0, particleSystemHeight * 2.0).onChange(onParametersUpdate);
-    controls.add(parameters, 'radiusX', 0, 10).onChange(onParametersUpdate);
-    controls.add(parameters, 'radiusZ', 0, 10).onChange(onParametersUpdate);
-    controls.add(parameters, 'size', 1, 400).onChange(onParametersUpdate);
-    controls.add(parameters, 'scale', 1, 30).onChange(onParametersUpdate);
-    controls.add(parameters, 'opacity', 0, 1).onChange(onParametersUpdate);
-    controls.add(parameters, 'speedH', 0.1, 3).onChange(onParametersUpdate);
-    controls.add(parameters, 'speedV', 0.1, 3).onChange(onParametersUpdate);
+      controls.addColor(parameters, 'color').onChange(onParametersUpdate);
+      controls.add(parameters, 'height', 0, particleSystemHeight * 2.0).onChange(onParametersUpdate);
+      controls.add(parameters, 'radiusX', 0, 10).onChange(onParametersUpdate);
+      controls.add(parameters, 'radiusZ', 0, 10).onChange(onParametersUpdate);
+      controls.add(parameters, 'size', 1, 400).onChange(onParametersUpdate);
+      controls.add(parameters, 'scale', 1, 30).onChange(onParametersUpdate);
+      controls.add(parameters, 'opacity', 0, 1).onChange(onParametersUpdate);
+      controls.add(parameters, 'speedH', 0.1, 3).onChange(onParametersUpdate);
+      controls.add(parameters, 'speedV', 0.1, 3).onChange(onParametersUpdate);
+    }
 
     function onMouseWheel(e) {
       e.preventDefault();
 
       if (e.wheelDelta) {
-        cameraZ += e.wheelDelta * 0.05;
+        cameraZ += e.wheelDelta * 0.02;
       } else if (e.detail) {
-        cameraZ += e.detail * 0.5;
+        cameraZ += e.detail * 0.2;
       }
     }
 
     if (mouseControls) {
 
       document.addEventListener('mousemove', function(e) {
-        var mouseX  = e.clientX, mouseY = e.clientY, width = window.innerWidth, halfWidth = width >>
-          1, height = window.innerHeight, halfHeight = height >> 1;
+        var mouseX  = e.clientX, mouseY = e.clientY, width = windowWidth, halfWidth = width >>
+          1, height = windowHeight, halfHeight = height >> 1;
 
         cameraX = cameraRadius * ( mouseX - halfWidth ) / halfWidth;
         cameraY = cameraRadius * ( mouseY - halfHeight ) / halfHeight;
@@ -183,33 +199,43 @@ angular.module('angular-snow', []).directive('ngSnow', function() {
 
   return {
     scope:      {
-      ngSnow:        '=',
-      flake:         '@',
+      flake:         '=',
       bgColor:       '@',
       mouseControls: '=',
+      gui:           '=',
       flakeCount:    '=',
       scale:         '@'
     },
-    restrict:   'A',
+    restrict:   'E',
     replace:    true,
     transclude: true,
-    template:   '<div  class="angular-snow" ng-show="ngSnow"><ng-transclude></ng-transclude></div>',
+    template:   '<div class="angular-snow"><ng-transclude></ng-transclude></div>',
     link:       function(scope, el, attrs) {
 
       var defaultFlake = "flake.png";
       scope.flake = scope.flake || defaultFlake;
       scope.mouseControls = scope.mouseControls || false;
-      scope.flakeCount = scope.flakeCount || 5000;
+      scope.flakeCount = scope.flakeCount || 2000;
       scope.scale = scope.scale || 4.0;
 
-      window.onload = function() {
-        init(el, scope.flake, scope.flakeCount, scope.mouseControls, scope.scale);
-        animate();
-      };/*
+      /*window.onload = function() {
+       init(el, scope.flake, scope.flakeCount, scope.mouseControls, scope.gui, scope.scale);
+       animate();
+       };*/
+      /*scope.$watch('flake', function(flake) {
+       console.debug('changed flake');
+       init(el, scope.flake, scope.flakeCount, scope.mouseControls, scope.gui, scope.scale);
+       });*/
 
-      scope.$watchGroup(['flake', 'mouseControls', 'flakeCount', 'scale'], function(params) {
-        console.debug(params);
-      });*/
+      scope.$watchGroup([
+        'mouseControls',
+        'flakeCount',
+        'flake',
+        'scale'], function(params) {
+        //clearCanvas();
+        init(el, scope.flake, scope.flakeCount, scope.mouseControls, scope.gui, scope.scale);
+        animate();
+      });
     }
   };
 });
